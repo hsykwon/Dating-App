1207e432eb43a31adddaa075065d315d9438f530
hsykwon <hsykwon@gmail.com>
2017-10-01 20:02:30 -0700

    font butt

--- static/main.js
+++ static/main.js
@@ -1,16 +1,26 @@
 window.onload = function() {
-	if (localStorage.getItem('input') !== null) {
-		document.getElementById('display').innerHTML = localStorage.getItem('input');
+	if (localStorage.getItem('input1') !== null) {
+		document.getElementById('display1').innerHTML = localStorage.getItem('input1');
 	}
-
+	if (localStorage.getItem('input2') !== null) {
+		document.getElementById('display2').innerHTML = localStorage.getItem('input2');
+	}
+	if (localStorage.getItem('input3') !== null) {
+		document.getElementById('display3').innerHTML = localStorage.getItem('input3');
+	}
 }
 
-function storeData() {
+function storeData1() {
+	document.getElementById('display1').innerHTML = document.getElementById('anniv').value;
+	localStorage.setItem("input1", document.getElementById('anniv').value);
+}
 
-	// console.log(localStorage.getItem("input"));
-	document.getElementById('display').innerHTML = document.getElementById('firstDate').value;
-	// storage.removeItem('input');
-	localStorage.setItem("input", document.getElementById('firstDate').value);
-	console.log("A");
-	console.log(localStorage.getItem("input"));
+function storeData2() {
+	document.getElementById('display2').innerHTML = document.getElementById('firstDate').value;
+	localStorage.setItem("input2", document.getElementById('firstDate').value);
+}
+
+function storeData3() {
+	document.getElementById('display3').innerHTML = document.getElementById('favMem').value;
+	localStorage.setItem("input3", document.getElementById('favMem').value);
 }
